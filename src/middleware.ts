8cbd010003fa1fcf15895b634e2d55a063894ef8
Y.K <powdersugar828828@gmail.com>
2024-01-22 16:39:46 +0900

    first commit

---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+
+/**
+ * @see https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+ */
+export const config = {
+  matcher: ["/:path*", "/index/:path*"]
+};
+
+export function middleware(req: NextRequest) {
+  if (process.env.VERCEL_ENV !== "production") {
+    const basicAuth = req.headers.get("Authorization");
+
+    if (basicAuth) {
+      const authValue = basicAuth.split(" ")[1];
+      // atob is deprecated but Buffer.from is not available in Next.js edge.
+      const [user, password] = atob(authValue).split(":");
+
+      if (user === "test" && password === "test") {
+        return NextResponse.next();
+      }
+
+      return NextResponse.json(
+        { error: "Invalid credentials" },
+        {
+          headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
+          status: 401
+        }
+      );
+    } else {
+      return NextResponse.json(
+        { error: "Please enter credentials" },
+        {
+          headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
+          status: 401
+        }
+      );
+    }
+  }
+}
